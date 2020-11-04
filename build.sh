0cc5d2fa1998945c33aa3500d112544a9f9c0b1a
唐钰豆豆 <doudoudzj@qq.com>
2020-11-04 10:40:15 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,8 @@ pyinstaller\
     -w\
     build.spec
 
+# or shell
 # pyinstaller -F -w --clean -y mac_show_all_files.py
+
+# sign app
+sudo codesign --force --deep --sign - ./dist/MacShowAllFiles.app
